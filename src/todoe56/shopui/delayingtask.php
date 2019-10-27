20c4b640df11e238dba914a89c34dade252aa0a0
notacoderr <38270548+notacoderr@users.noreply.github.com>
2019-10-27 23:37:06 +0400

    Create delayingtask.php

---
+++ src/todoe56/shopui/delayingtask.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace todoe56\shopui;
+
+use pocketmine\scheduler\Task;
+use pocketmine\item\Item;
+use pocketmine\Player;
+
+use todoe56\shopui\{shopui as Main, \commands\ShopCommand};
+
+class delayingtask extends Task {
+
+	private $plugin, $time;
+
+	public function __construct(Main $plugin, Item $itemClicked, Player $player) {
+		$this->plugin = $plugin;
+	}
+
+	public function onRun($currentTick)
+  {
+  var_dump($currentTick);
+     if($currentTick === 1000) {
+       ShopCommand::openShop($itemClicked->getName(), $player);
+     }
+  }
+}
